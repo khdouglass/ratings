bb3ada6e68e533357bb935020975b546045f2ef4
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-02 13:00:37 -0700

    added html, updated server

--- server.py
+++ server.py
@@ -2,10 +2,11 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask, jsonify
+from flask import (Flask, render_template, redirect, request, flash,
+                   session)
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -22,9 +23,23 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    a = jsonify([1,3])
-    return a
+    # a = jsonify([1,3])
+    # return a
 
+    return render_template("homepage.html")
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+@app.route("/register")
+def register_form():
+    email = request.args.get("email")
+    password = request.args.get("password")
+
+    return render_template("register_form.html", email=email, password=password)
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
